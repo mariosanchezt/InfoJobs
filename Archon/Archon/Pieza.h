8acd15ed3cdedcf0253a98b8adb288204403c79b
dcm002 <daniel.castro.martinez@alumnos.upm.es>
2026-04-05 18:42:00 +0200

    Pieza.h bien

--- Archon/Archon/Pieza.h
+++ Archon/Archon/Pieza.h
@@ -1,1 +1,35 @@
 #pragma once
+#include <string>
+
+// Los tres tipos de movimiento
+enum TipoMovimiento { GROUND, FLYING, TELEPORT };
+
+class Pieza {
+public:
+    // Atributos numéricos de combate y tablero
+    float vida;
+    float fuerza;
+    float velAtaque;
+    float intervaloAtaque; // Tiempo de recarga
+    int radioMovimiento;
+
+    // Casilla inicial (coordenadas en el tablero 9x9) 
+    int filaInicial;
+    int colInicial;
+
+    TipoMovimiento mov; // Tierra, vuelo o teleport
+
+    // Constructor: aquí "rellenas" la ficha con sus estadísticas
+    Pieza(float v, float f, float va, float intA, int rm, int fIni, int cIni, TipoMovimiento t) {
+        vida = v;
+        fuerza = f;
+        velAtaque = va;
+        intervaloAtaque = intA;
+        radioMovimiento = rm;
+        filaInicial = fIni;
+        colInicial = cIni;
+        mov = t;
+    }
+
+    virtual ~Pieza() {} // Necesario para que C++ gestione bien la memoria
+};
