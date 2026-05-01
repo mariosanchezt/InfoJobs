88a7bd7a4ae69986a8df8c96a2d503caadb268d7
mariosanchezt <mario.sanchez.trigo@alumnos.upm.es>
2026-05-01 19:50:02 +0200

    Menu con fondo guapo
    Co-Authored-By: JaviSM03 <100871114+JaviSM03@users.noreply.github.com>

--- Archon/Archon/Menu.h
+++ Archon/Archon/Menu.h
@@ -8,7 +8,7 @@ enum class EstadoJuego { MENU, CARGANDO, JUGANDO_LOCAL, JUGANDO_IA };
 struct Boton {
     sf::RectangleShape forma;
     std::string texto;
-    bool habilitado;
+    bool habilitado = false;
 };
 
 class Menu {
